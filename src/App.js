07bb680f1fd0450b82e0c461842ac7362a51a4a0
sanyupaulreddy <158132555+sanyupaulreddy@users.noreply.github.com>
2025-03-13 15:34:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NewsMedia from './components/First';
+import './components/First.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <NewsMedia/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/First.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./First.css"
+
+const NewsMedia=()=>{
+    
+
+
+return(
+    <div className="hello">
+        <h1>NEWS MEDIA PLATFORM</h1>
+        <div className="hi">
+            <button>VIEW</button>
+            <button>ADD</button>
+            <button>DELETE</button>
+        </div>
+    </div>
+)};
+export default NewsMedia
